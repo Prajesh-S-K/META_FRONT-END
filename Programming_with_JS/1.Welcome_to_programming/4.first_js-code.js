ba4e704a39ad701d78029707d9957c31da5218b6
Prajesh-S-K <prajeshsivaprakash@gmail.com>
2024-08-18 13:16:48 -0400

    2/1.1

---
+++ Programming_with_JS/1.Welcome_to_programming/4.first_js-code.js
@@ -0,0 +1,15 @@
+//this is a comment
+
+/*
+this is a multi-line comment
+*/
+
+// useful shortcut shift + enter to move to the next line
+
+console.log("Hello World"); // normal output
+
+console.log("%cHello World", "color: red; font-size: 40px;"); // output with style
+//the output will be in red color and font size will be 40px only in a browser console
+
+console.log("hello", "world"); // multiple arguments
+console.log("hello" + "world"); // concatenation
